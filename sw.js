6829a785155205d2d0386384e64deff4c5fa7113
AdamReynolds2 <airadam92@gmail.com>
2025-06-10 08:21:04 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('twaudio-cache-v1').then((cache) =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png',
+        './favicon.ico'
+      ])
+    )
+  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
